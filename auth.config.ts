6976be069dbfda045eae5677a966b9ed3fff1522
techzealot <techzealot@foxmail.com>
2024-07-13 03:58:59 +0800

    make callbackurl effect

--- auth.config.ts
+++ auth.config.ts
@@ -7,12 +7,13 @@ export const authConfig = {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
             const isOnDashBoard = nextUrl.pathname.startsWith("/dashboard");
+            const isOnLogin = nextUrl.pathname.startsWith("/login");
             if (isOnDashBoard) {
                 if (isLoggedIn) {
                     return true;
                 }
                 return false;
-            } else if (isLoggedIn) {
+            } else if (isLoggedIn && isOnLogin) {
                 return Response.redirect(new URL("/dashboard", nextUrl))
             }
             return true;
